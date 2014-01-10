06632e5d3fb768bc12ad342918ee0ffd14b3b027
Francisco ALberini <f.alberini@gmail.com>
2014-01-10 15:18:36 -0800

    Installing new rails project and adding gems incl Postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learncodebeta::Application.config.secret_token = 'df6bd7b8dd283d4eedb14d2977ca2c4eda933df6ba5feebe4007d96b09c2d6df92abad96027d32a1f23808c3cb68c32a3c76e16cd46ebaf5efa1c5d9bed2b237'
