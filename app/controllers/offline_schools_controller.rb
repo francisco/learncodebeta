600ed826040d99a29a027640966d153178ffde98
Francisco ALberini <f.alberini@gmail.com>
2014-01-12 16:32:02 -0800

    Adding logic to offline schools controller

--- app/controllers/offline_schools_controller.rb
+++ app/controllers/offline_schools_controller.rb
@@ -1,6 +1,27 @@
 class OfflineSchoolsController < ApplicationController
 
   def index
+    search_query = params[:address_search]
+    if search_query
+      query = URI::escape(search_query)
+      result = Typhoeus.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{query}&sensor=true")
+      result_hash = JSON.parse(result.body)
+      result_hash["results"].each do |result|
+        @lat = result["geometry"]["location"]["lat"]
+        @lng = result["geometry"]["location"]["lng"]
+      end
+    end
+    respond_to do |format|
+      format.html
+      format.json {
+        # lat = params[:lat]
+        # lng = params[:lng]
+        render json: {
+          lat: @lat,
+          lng: @lng
+        }
+      }
+    end
   end
 
   def new
--- app/helpers/offline_schools_helper.rb
+++ app/helpers/offline_schools_helper.rb
@@ -1,2 +1,11 @@
 module OfflineSchoolsHelper
+  def getLatlng(search_query)
+    query = URI::escape(search_query)
+    result = Typhoeus.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{query}&sensor=true")
+    result_hash = JSON.parse(result.body)
+    result_hash["results"].each do |result|
+      @lat = result["geometry"]["location"]["lat"]
+      @lng = result["geometry"]["location"]["lng"]
+    end
+  end
 end
