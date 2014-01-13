7bc4a59c6bcb563650d7623e909753156c331535
Francisco ALberini <f.alberini@gmail.com>
2014-01-12 16:32:24 -0800

    Adding JS to render map and update location based on search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,56 +17,56 @@
 //= require_tree .
 
 $(document).ready(function() {
-  var map = L.map('map').setView([51.505, -0.09], 13);
-  L.tileLayer('http://{s}.tile.cloudmade.com/f6c127adc91a47f1b4a73837013ed783/998/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+  event.preventDefault();
+  var map = L.map('map', {
+      center:[37.7749295,-122.4194155],
+      zoom: 11,
+    // layers: [school_group]
+    });
+    L.tileLayer('http://{s}.tile.cloudmade.com/f6c127adc91a47f1b4a73837013ed783/998/256/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
       maxZoom: 18,
       minZoom: 1,
       maxBounds: [[-85, -180], [85, 180]]
     }).addTo(map);
+
+  $('#submit_search').click(function(){
+    var address_search = $('#address_search').val();
+    updateMap(map, address_search);
+  })
+  // var school_group = new L.LayerGroup();
+  // var url = "/offline_schools.json?lat=37.7749295&lng=-122.4194155";
+  createMap(map);
+  // var overlays = {
+  //   "school_group": school_group
+  // }
+  // L.control.layers(overlays).addTo(map);
 });
 
-// var createMap = function(map){
-//   var map = L.map('map', {
-//     center:[37.7749295,-122.4194155],
-//     zoom: 11,
-//     layers: [school_group]
-//   });
+var updateMap = function(map, address_search){
+  $.ajax({
+    type: "GET",
+    url: "/offline_schools.json?address_search=" + address_search
+  }).done(function (data) {
+    var lat = data.lat;
+    var lng = data.lng;
+    map.setView(new L.LatLng(lat, lng));
+    // layers: [school_group]
+  });
+};
 
-//   L.tileLayer('http://{s}.tile.cloudmade.com/f6c127adc91a47f1b4a73837013ed783/998/256/{z}/{x}/{y}.png', {
-//   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-//     maxZoom: 18,
-//     minZoom: 1,
-//     maxBounds: [[-85, -180], [85, 180]]
-//   }).addTo(map);
-//   getMarkers(url, school_group)
-// }
+var createMap = function(map){
+  var address_search = address_search;
+  $.ajax({
+    type: "GET",
+    url: "/offline_schools.json"
+  }).done(function (data) {
+    // for (var i = 0; i < response.length; i++) {
+    //   L.marker([response[i].latitude,response[i].longitude])
+    //     .openPopup('click').bindPopup(
+    //     "<strong>" + response[i]["name"] + "</strong>" + "<br>"
+    //     + response[i]["street"] + "<br>" + response[i]["bio"]
+    //   ).addTo(school_group);
+  });
+};
 
-// $(function(){
-//   var school_group = new L.LayerGroup();
-//   var url = "/offline_schools.json?lat=37.7749295&lng=-122.4194155";
-
-//   var getMarkers = function(url, school_group){
-//   $.ajax({
-//       type: "GET",
-//       url: url,
-//       dataType: 'json',
-//       success: function (response) {
-//         console.log(response)
-//         for (var i = 0; i < response.length; i++) {
-//           L.marker([response[i].latitude,response[i].longitude])
-//           .openPopup('click').bindPopup(
-//             "<strong>" + response[i]["name"] + "</strong>" + "<br>"
-//             + response[i]["street"] + "<br>" + response[i]["bio"]
-//           ).addTo(school_group);
-//         }
-//       }
-//   });
-// }
-// createMap();
-// var overlays = {
-//                 "school_group": school_group
-//         }
-//         L.control.layers(overlays).addTo(map);
-
-// });
